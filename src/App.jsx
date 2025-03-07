72b67c4ff9e48f7531ed2a958ba958df151a21e4
Mansoursamb <sambmansour.ms@gmail.com>
2025-03-07 11:50:25 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import Header from "./composants/Header";
+
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ src/composants/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "../../public/img/argentBankLogo.png"; // Import de l'image
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <nav className="main-nav">
+      <a className="main-nav-logo" href="./index.html">
+        <img
+          className="main-nav-logo-image"
+          src={logo} // Utilisation de l'import de l'image
+          alt="Argent Bank Logo"
+        />
+        <h1 className="sr-only">Argent Bank</h1>
+      </a>
+      <div>
+        <a className="main-nav-item" href="./sign-in.html">
+          <i className="fa fa-user-circle"></i>
+          Sign In
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
